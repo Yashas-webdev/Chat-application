fef955c5fd948fdfaa2b9b3bb9cbe4d7842db450
Yashas-webdev <yashasaryashu@gmail.com>
2026-06-04 00:12:08 +0530

    created the userRoute.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
-
+import userRouter from "./routes/userRoute.js"
 dotenv.config({});
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/v1/user",userRouter)
 
 app.listen(PORT,()=>{
     connectDB();
