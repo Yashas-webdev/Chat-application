5fd44afa6c56ba7a48f00263fb092d6ea96a1d3e
Yashas-webdev <yashasaryashu@gmail.com>
2026-06-25 00:03:08 +0530

    created the link to the login page if the account is already created

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,82 +1,124 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
     <>
-    <div className='flex justify-center items-center min-h-screen'>
-        <div className='w-full max-w-sm p-6 rounded-lg
+      <div className="flex justify-center items-center min-h-screen">
+        <div
+          className="w-full max-w-sm p-6 rounded-lg
                 bg-white/10
                 backdrop-blur-md
                 border border-white/20
-                shadow-md'>
-          <h1 className='text-3xl font-bold text-center'>SignUp</h1>
+                shadow-md"
+        >
+          <h1 className="text-3xl font-bold text-center">SignUp</h1>
 
-          <form action=''>
-            <div className='mt-4'>
-              <label className='label p-2'>
-                <span className='label-text'>Full Name</span>
+          <form action="">
+            <div className="mt-4">
+              <label className="label p-2">
+                <span className="label-text">Full Name</span>
               </label>
-              <input type='text' placeholder='Full Name' className='input w-full
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="input w-full
              bg-white/10
              backdrop-blur-md
              border-white/20
              text-white
-             placeholder-gray-300'/>
+             placeholder-gray-300"
+              />
             </div>
 
-            <div className='mt-4'>
-              <label className='label p-2'>
-                <span className='label-text'>User name</span>
+            <div className="mt-4">
+              <label className="label p-2">
+                <span className="label-text">User name</span>
               </label>
-              <input type='text' placeholder='username' className='input w-full
+              <input
+                type="text"
+                placeholder="username"
+                className="input w-full
              bg-white/10
              backdrop-blur-md
              border-white/20
              text-white
-             placeholder-gray-300'/>
+             placeholder-gray-300"
+              />
             </div>
 
-            <div className='mt-4'>
-              <label className='label p-2'>
-                <span className='label-text'>Password</span>
+            <div className="mt-4">
+              <label className="label p-2">
+                <span className="label-text">Password</span>
               </label>
-              <input type='password' placeholder='*******' className='input w-full
+              <input
+                type="password"
+                placeholder="*******"
+                className="input w-full
              bg-white/10
              backdrop-blur-md
              border-white/20
              text-white
-             placeholder-gray-300'/>
+             placeholder-gray-300"
+              />
             </div>
 
-            <div className='mt-4'>
-              <label className='label p-2'>
-                <span className='label-text'>Confirm Password</span>
+            <div className="mt-4">
+              <label className="label p-2">
+                <span className="label-text">Confirm Password</span>
               </label>
-              <input type='password' placeholder='*******' className='input w-full
+              <input
+                type="password"
+                placeholder="*******"
+                className="input w-full
              bg-white/10
              backdrop-blur-md
              border-white/20
              text-white
-             placeholder-gray-300'/>
+             placeholder-gray-300"
+              />
             </div>
 
-            <div className='flex items-center my-4'>
-              <div className='flex items-center'>
+            <div className="flex items-center my-4">
+              <div className="flex items-center">
                 <p>Male</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+                <input
+                  type="checkbox"
+                  defaultChecked
+                  className="checkbox mx-2"
+                />
               </div>
 
-              <div className='flex items-center'>
+              <div className="flex items-center">
                 <p>Female</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+                <input
+                  type="checkbox"
+                  defaultChecked
+                  className="checkbox mx-2"
+                />
               </div>
-            </div>   
-            
+            </div>
+
+            <Link to="/login">Already have an account?</Link>
+
+            <div className="mt-2">
+             <button className="input w-full
+             bg-white/10
+             backdrop-blur-md
+             border-white/10
+             text-white
+             placeholder-gray-300
+             flex justify-center items-center
+             cursor-pointer
+             py-1 rounded-md">
+              SignUp
+             </button>
+            </div>
           </form>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
