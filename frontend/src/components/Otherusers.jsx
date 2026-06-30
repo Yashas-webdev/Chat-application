8e0413bb96a737a21bc02449659cae835f846ca1
Yashas-webdev <yashasaryashu@gmail.com>
2026-06-30 10:08:16 +0530

    created the otherusers.jsx and added the avatar div

---
+++ frontend/src/components/Otherusers.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Otherusers() {
+  return (
+    <div>
+        <div>
+            <div>
+                <div className='avatar online' >
+                    <div className='w-10 rounded-full backdrop-blur-lg cursor-pointer'>
+                        <img src='https://img.magnific.com/premium-vector/cool-cartoon-boy-avatar_987671-675.jpg?semt=ais_hybrid&w=740&q=80' alt='User-profile'/>
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Otherusers
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
+import Otherusers from "./Otherusers";
 
 function Sidebar() {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col">
       <form action="" className="flex items-center gap-2">
         <input
           className="input input-bordered rounded-md bg-white text-black placeholder:text-gray-500"
@@ -15,9 +16,8 @@ function Sidebar() {
         </button>
       </form>
 
-      <div>
-        
-      </div>
+      <div className="divider px-3"></div>
+        <Otherusers/>
     </div>
   );
 }
