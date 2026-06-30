ee8383082a1605b8cec2a2318386e8acaeb221b4
Yashas-webdev <yashasaryashu@gmail.com>
2026-06-30 13:34:07 +0530

    SendInput.jsx component is created for the send button and input field for the message

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import SendInput from "./SendInput";
 
 function MessageContainer() {
   return (
-    <div className='md:min-w-[450px] flex flex-col p-1 '>
-     
-        <div className='flex gap-3 items-center bg-zinc-800 text-amber-50 p-2 border rounded border-amber-50 '>
-                <div className='avatar online'>
-                    <div className='w-10 rounded-full'>
-                        <img src='https://img.magnific.com/premium-vector/cool-cartoon-boy-avatar_987671-675.jpg?semt=ais_hybrid&w=740&q=80' alt='User-profile'/>
-                    </div>
-                </div>
-            
-            <div className='flex flex-col flex-1'> 
-                <div className='flex  justify-between gap-2 flex-1 '>
-                        <p>Yashas MERNstack</p>
-                </div>
-            </div>
+    <div className="md:min-w-[450px] flex flex-col p-1 ">
+      <div className="flex gap-3 items-center bg-zinc-800 text-amber-50 px-4 py-2 mb-2 border rounded border-zinc-700 ">
+        <div className="avatar online">
+          <div className="w-10 rounded-full">
+            <img
+              src="https://img.magnific.com/premium-vector/cool-cartoon-boy-avatar_987671-675.jpg?semt=ais_hybrid&w=740&q=80"
+              alt="User-profile"
+            />
+          </div>
         </div>
-    
+
+        <div className="flex flex-col flex-1">
+          <div className="flex  justify-between gap-2 flex-1 ">
+            <p>Yashas MERNstack</p>
+          </div>
+        </div>
+      </div>
+      <SendInput/>
     </div>
-  )
+  );
 }
 
 export default MessageContainer;
---
+++ frontend/src/components/SendInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { RiSendPlaneFill } from "react-icons/ri";
+function SendInput() {
+  return (
+    <form className='px-3 my-3'>
+        <div className='w-full relative '>
+            <input 
+             type='text'
+             placeholder='Send a message...'
+             className='border text-sm rounded-lg p-3 block w-full border-zinc-500 bg-gray-500 text-white'
+            />
+            <button className='absolute flex inset-y-0 end-0 items-center pr-4'>
+                <RiSendPlaneFill />
+            </button>
+        </div>
+    </form>
+  )
+}
+
+export default SendInput
