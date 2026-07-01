121805082531e1f583f350cc464c0a1c13fcca23
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-02 00:41:08 +0530

    got the user other hooks stored in the redux store and using the dispatch it is used in the Otherusers.jsx and using <Otheruser/> it is used as the component

--- frontend/src/components/Otherusers.jsx
+++ frontend/src/components/Otherusers.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import Otheruser from './Otheruser'
 import useGetOtherUsers from '../hooks/useGetOtherUsers'
+import { useSelector } from 'react-redux';
 
 function Otherusers() {
-
+  //my custom hook
   useGetOtherUsers();
+  const {otherUsers} = useSelector(Store => Store.user);
+  if(!otherUsers) return; //early return in react
   return (
     <div className='overflow-auto'>
-        <Otheruser/>
-        <Otheruser/>
-        <Otheruser/>
-        <Otheruser/>
-        <Otheruser/>
-        <Otheruser/>
-        <Otheruser/>
-        <Otheruser/>
-        <Otheruser/>
+      {
+      
+        otherUsers?.map((user)=>{
+          return(
+            <Otheruser key={user._id} user={user}/>
+          )
+        })
+
+      }
     </div>
   )
 }
