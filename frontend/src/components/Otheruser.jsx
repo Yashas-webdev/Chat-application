7e4f19936c7f9a584afcd83f283ca737eede65f0
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-02 00:48:29 +0530

    the user name and the profile photo given the value

--- frontend/src/components/Otheruser.jsx
+++ frontend/src/components/Otheruser.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 
-function Otheruser() {
+function Otheruser(props) {
+
+    const user = props.user
   return (
     <div>
         <div className='flex gap-3 items-center text-white hover:text-zinc-900 hover:bg-zinc-200 rounded p-2 cursor-pointer'>
                 <div className='avatar avatar-online'>
                     <div className='w-10 rounded-full'>
-                        <img src='https://img.magnific.com/premium-vector/cool-cartoon-boy-avatar_987671-675.jpg?semt=ais_hybrid&w=740&q=80' alt='User-profile'/>
+                        <img src={user?.profilePhoto} alt='User-profile'/>
                     </div>
                 </div>
             
             <div className='flex flex-col flex-1'> 
                 <div className='flex  gap-2 flex-1'>
-                        <p>Yashas MERNstack</p>
+                        <p>{user?.fullName}</p>
                 </div>
             </div>
         </div>
