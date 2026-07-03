b4c470f42a3d8c49d0df84847cf4d45bf2dcb99d
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-03 22:57:01 +0530

    added the onclick handler to the Logout in the sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { BiSearchAlt2 } from "react-icons/bi";
 import Otherusers from "./Otherusers";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 function Sidebar() {
+  const navigate = useNavigate();
+  const logoutHandler = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:8080/api/v1/user/logout`,
+      );
+      navigate("/login");
+      toast.success(res.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="border-r border-slate-400 p-4 flex flex-col ">
       <form action="" className="flex items-center gap-2">
@@ -11,16 +27,24 @@ function Sidebar() {
           type="text"
           placeholder="Search..."
         />
-        <button type="submit" className="btn bg-zinc-700 hover:bg-zinc-500 text-white border-amber-50">
+        <button
+          type="submit"
+          className="btn bg-zinc-700 hover:bg-zinc-500 text-white border-amber-50"
+        >
           <BiSearchAlt2 className="w-6 h-6 outline-none" />
         </button>
       </form>
 
       <div className="divider px-3 "></div>
-        <Otherusers/>
-        <div className="mt-2">
-          <button className="btn btn-sm text-white hover:text-zinc-900 hover:bg-white cursor-pointer ">Logout</button>
-        </div>
+      <Otherusers />
+      <div className="mt-2">
+        <button
+          onClick={logoutHandler}
+          className="btn btn-sm text-white hover:text-zinc-900 hover:bg-white cursor-pointer "
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
