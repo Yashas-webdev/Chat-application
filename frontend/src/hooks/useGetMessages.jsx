c713002b83d197d291d05c693f21475994c0b654
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-04 19:28:41 +0530

    created the useGetMessages.jsx hooks in the hooks folder

---
+++ frontend/src/hooks/useGetMessages.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setSetlectedUser } from '../redux/userSlice'
+const useGetMessages =  () =>{
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        const fetchMessages = async()=>{
+            try{
+                axios.defaults.withCredintials = true;
+                const res = await axios.get(`http://localhost:8080/api/v1/message/6a246eeae17b23ef63660ccd`);
+                //store
+                dispatch(setSetlectedUser(res.data))
+            }catch(error){
+
+            }
+        }
+    },[])
+}
+export default useGetMessages
--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -5,6 +5,7 @@ const userSlice = createSlice({
     initialState:{
         authUser:null,
         otherUsers:null,
+        selectedUser:null,
     },
     reducers:{
         setAuthUser:(state,action)=>{
@@ -12,9 +13,12 @@ const userSlice = createSlice({
         },
         setOtherUsers:(state,action)=>{
             state.otherUsers = action.payload
+        },
+        setSetlectedUser:(state,action)=>{
+            state.selectedUser = action.payload
         }
     }
 });
 
-export const {setAuthUser,setOtherUsers} = userSlice.actions
+export const {setAuthUser,setOtherUsers,setSetlectedUser} = userSlice.actions
 export default userSlice.reducer;
