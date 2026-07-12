5f8494bf72f2dc1765ac83ee112903503777707c
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-13 00:43:03 +0530

    I integrated Socket.IO with my Express backend by attaching it to the HTTP server instead of using app.listen(). I implemented a userSocketMap to map user IDs to socket IDs, passed the authenticated user's ID during the socket handshake, fixed event name mismatches between the client and server, and updated my Redux slice to manage online users correctly. After these changes, I was able to establish real-time socket connections and maintain an accurate list of online users.

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -1,34 +1,44 @@
 import { Server } from "socket.io";
 import http from "http";
 
-
 let io;
+const userSocketMap = {}; // Store online users
 
 const initializeSocket = (app) => {
-  const server = http.createServer(app);
+    const server = http.createServer(app);
 
-  io = new Server(server, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
-  });
+    io = new Server(server, {
+        cors: {
+            origin: "http://localhost:5173",
+            methods: ["GET", "POST"],
+            credentials: true,
+        },
+    });
 
-  io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-  
-  const userId = socket.handshake.query.userId
-  if(userId !== undefined){
-    userSocketMap[userId] = socket.id;
-  }
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-    delete userSocketMap[userId];
-    io.emit('getOnlineUser',Object.keys(userSocketMap));
-  });
-});
-  return server;
+    io.on("connection", (socket) => {
+        console.log("User connected:", socket.id);
+
+        const userId = socket.handshake.query.userId;
+
+        if (userId) {
+            userSocketMap[userId] = socket.id;
+        }
+
+        // Send updated online users
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected:", socket.id);
+
+            if (userId) {
+                delete userSocketMap[userId];
+            }
+
+            io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        });
+    });
+
+    return server;
 };
 
-export { initializeSocket, io };
+export { initializeSocket, io, userSocketMap };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,87 +1,83 @@
-import './App.css'
+import "./App.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import Signup from './components/signup';
-import Login from './components/Login';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { setAuthUser } from './redux/userSlice';
-import axios from 'axios';
-import io from 'socket.io-client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Signup from "./components/signup";
+import Login from "./components/Login";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { setAuthUser, setOnlineUsers } from "./redux/userSlice";
+import { setSocket } from "./redux/socketSlice";
+import axios from "axios";
+import io from "socket.io-client";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />
-  },
-  {
-    path: "/Signup",
-    element: <Signup />
-  },
-  {
-    path: "/login",
-    element: <Login />
-  }
+    {
+        path: "/",
+        element: <HomePage />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
 ]);
 
 function App() {
+    const { authUser } = useSelector((store) => store.user);
+    const dispatch = useDispatch();
 
-  const { authUser } = useSelector(store => store.user);
-  const dispatch = useDispatch();
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const res = await axios.get(
+                    "http://localhost:8080/api/v1/user/me",
+                    {
+                        withCredentials: true,
+                    }
+                );
 
-  const dispatch = useDispatch();
+                dispatch(setAuthUser(res.data));
+            } catch (error) {
+                console.log(error);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:8080/api/v1/user/me",
-          {
-            withCredentials: true,
-          }
-        );
+                if (error.response?.status === 401) {
+                    dispatch(setAuthUser(null));
+                }
+            }
+        };
 
-        dispatch(setAuthUser(res.data));
+        fetchUser();
+    }, [dispatch]);
 
-      } catch (error) {
-        console.log(error);
+    useEffect(() => {
+        if (authUser) {
+            const newSocket = io("http://localhost:8080", {
+                query: {
+                    userId: authUser._id,
+                },
+            });
 
-        if (error.response?.status === 401) {
-          dispatch(setAuthUser(null));
+            dispatch(setSocket(newSocket));
+
+            newSocket.on("getOnlineUsers", (onlineUsers) => {
+                dispatch(setOnlineUsers(onlineUsers));
+            });
+
+            return () => {
+                newSocket.disconnect();
+            };
         }
-      }
-    };
+    }, [authUser, dispatch]);
 
-    fetchUser();
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (authUser) {
-      const newSocket = io("http://localhost:8080",{
-        query:{
-          userId:authUser._id
-        }
-      });
-
-
-      dispatch(setSocket(newSocket));
-
-      Socket.on('getOnlineUsers',(onlineUsers)=>{
-        dispatch(setOnelineUsers(onlineUsers))
-      })
-
-      return () => {
-        newSocket.disconnect();
-      };
-    }
-  }, [authUser]);
-
-  return (
-    <div className="p-4 h-screen flex items-center justify-center">
-      <RouterProvider router={router} />
-    </div>
-  );
+    return (
+        <div className="p-4 h-screen flex items-center justify-center">
+            <RouterProvider router={router} />
+        </div>
+    );
 }
 
 export default App;
