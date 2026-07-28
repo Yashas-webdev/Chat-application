9f56421eabdf06fed4b287fbe9de26c747817f5a
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-28 23:58:37 +0530

    created the protectedRouter to avoid the entering to the home page before loging

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react"
+import {Navigate} from 'react-router-dom'
+
+
+const  ProtectedRoute = ({children}) =>{
+  const {authUser} = useSelector((store)=>store.user);
+
+  if(!authUser){
+    return <Navigate to ='/login' replace/>;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute
