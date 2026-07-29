2ffe8ba116ed284a6b4ec5a70ab1dbf5b81c2d92
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-30 00:37:13 +0530

    Configure CORS for Vercel frontend

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:'https://chatcom-wheat.vercel.app',
     credentials:true,
 };
 app.use(cors(corsOption));
--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -14,7 +14,7 @@ const initializeSocket = (app) => {
 
     io = new Server(server, {
         cors: {
-            origin: true,
+            origin: "https://chatcom-wheat.vercel.app",
             methods: ["GET", "POST"],
             credentials: true,
         },
