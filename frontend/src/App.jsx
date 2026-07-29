48334b3e6c1bac6b17fee83067a99a101766a1d3
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-30 00:28:33 +0530

    resolved the importing issue in the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import Signup from "./components/signup";
+import Signup from "./components/Signup";
 import Login from "./components/Login";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect,useState } from "react";
