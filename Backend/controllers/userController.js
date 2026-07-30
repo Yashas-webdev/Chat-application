55135ea2ad3fb01fc482d5e60cd3cf9a9bee84a5
Yashas-webdev <yashasaryashu@gmail.com>
2026-07-31 01:08:06 +0530

    Fix secure cookie option

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -78,7 +78,7 @@ export const login = async(req,res)=>{
 
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {expiresIn:'1d'});
 
-        return res.status(200).cookie("token",token, {maxAge:1*24*60*60*1000, httpOnly:true, sameSite:'none',secure:truncates}).send({
+        return res.status(200).cookie("token",token, {maxAge:1*24*60*60*1000, httpOnly:true, sameSite:'none',secure:true}).send({
             _id:user._id,
             username: user.username,
             fullName: user.fullName,
